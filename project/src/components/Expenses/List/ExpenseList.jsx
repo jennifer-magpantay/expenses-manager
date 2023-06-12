7f56b6fb2861106310e4c962757300808ac18232
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2023-06-12 15:25:25 +0200

    chore Set list component

---
+++ project/src/components/Expenses/List/ExpenseList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+import { ExpenseItem } from "./ExpenseItem";
+
+export const ExpenseList = ({ list, onClick }) => {
+  if (list.length === 0) {
+    return <p>No expenses found for the current year</p>;
+  }
+  return (
+    <>
+      <ul className="expenses--container">
+        {list.map(({ id, title, category, createdAt, amount }) => (
+          <li key={id}>
+            <ExpenseItem
+              id={id}
+              title={title}
+              category={category}
+              date={createdAt}
+              amount={amount}
+              onClick={onClick}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+ExpenseList.propTypes = {
+  list: PropTypes.array,
+  map: PropTypes.func,
+};
